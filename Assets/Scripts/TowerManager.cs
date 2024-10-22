4b8b7ac38a877ac4f677b76377bd66a567324712
OlegLesan <oleg.lesan1234@gmail.com>
2024-10-22 15:42:45 +0300

    14. Indicating placement position

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -6,20 +6,37 @@ public class TowerManager : MonoBehaviour
 {
     public static TowerManager instance;
 
+
     private void Awake()
     {
         instance = this;
     }
 
     public Tower activeTower;
-    
+    public Transform indicator;
+    public bool isPlacing;
 
-
+    void Update()
+    {
+        if(isPlacing)
+        {
+            indicator.position = GetGridPosition();
+        }
+    }
 
 
     public void StartTowerPlacement(Tower towerToPlace)
     {
         activeTower = towerToPlace;
         Debug.Log("Placing a tower");
+        isPlacing = true;
+        indicator.gameObject.SetActive(true);
+    }
+
+    public Vector3 GetGridPosition()
+    {
+        Vector3 location = new Vector3(2f, 0f, 2f);
+
+        return location;
     }
 }
